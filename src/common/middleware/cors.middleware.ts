588cd845012e6c9f565c423f03ba5d9afa3d9b21
Grigorii K. Shartsev <me@shgk.me>
2024-01-29 01:19:20 +0100

    fix(app): cors allow headers and methods

--- src/common/middleware/cors.middleware.ts
+++ src/common/middleware/cors.middleware.ts
@@ -4,8 +4,19 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction) {
-    response.header('Access-Control-Allow-Origin', request.headers.origin);
-    response.header('Access-Control-Allow-Credentials', 'true');
+    response.setHeader('Access-Control-Allow-Origin', request.headers.origin);
+    response.setHeader('Access-Control-Allow-Credentials', 'true');
+    response.setHeader('Access-Control-Allow-Headers', 'content-type');
+    response.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
+
+    if (request.method === 'OPTIONS') {
+      response.status(200).end();
+      return;
+    }
+
     next();
   }
 }
